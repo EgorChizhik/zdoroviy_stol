de71e1fa9857955ce1146c5337c1c3698d049d90
EgorChizhik <legend2019rus@gmail.com>
2025-12-25 16:46:01 +0400

    Защитил API от неавторизованных действий, установив IsAuthenticated вместо AllowAny по умолчанию.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -158,7 +158,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',  # ← ВРЕМЕННО ОТКЛЮЧАЕМ АВТОРИЗАЦИЮ НА ВСЁМ API
+        'rest_framework.permissions.IsAuthenticated', 
     ],
 }
 
